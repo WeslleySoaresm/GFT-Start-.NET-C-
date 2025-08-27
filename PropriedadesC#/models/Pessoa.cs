53cfc4a5b19e35b208f588fde3e9077cd5f65d9b
weslleysoares <correweslleysoares@gmail.com>
2025-08-27 19:12:21 +0100

    ClassPessoa

---
+++ PropriedadesC#/models/Pessoa.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dio.o_Estudos.PropriedadesC_.models.models
+{
+    public class Pessoa
+    {
+
+
+        public string Nome { get; set; }
+        public int idade { get; set; }
+    }
+}
